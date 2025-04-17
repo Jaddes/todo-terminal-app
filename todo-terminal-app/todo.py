d20471c32e40fb6d58c3ebddb5cce12668d102e7
Boris Lahos <borkolahos@gmail.com>
2025-04-17 19:15:08 +0200

    Creating a comment skeleton for project structure

---
+++ todo-terminal-app/todo.py
@@ -0,0 +1,12 @@
+# todo.py
+
+# 1. Define the Task Class
+# 2. Define the ToDoList class
+#    - add_task
+#    - list_tasks
+#    - mark_task_completed
+#    - delete_task
+#    - save_to_file
+#    - load_from_file
+# 3. Implement the command-line menu (while loop)
+# 4. Tie everything together (main function)
